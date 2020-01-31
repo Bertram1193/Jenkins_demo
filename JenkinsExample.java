ad6716da04979ada74c4bc699c8acf7f867dbbc7
Bertram1193 <59768402+Bertram1193@users.noreply.github.com>
2020-02-01 00:20:29 +0530

    Create JenkinsExample.java

---
+++ JenkinsExample.java
@@ -0,0 +1,10 @@
+public class JenkinsExample {
+
+ public static void main(String[] args) {
+  // TODO Auto-generated method stub
+    System.out.println("Hello Agni");
+    System.out.println("Hello Bertram");
+	System.out.println("Hello Russell");
+ }
+
+}
